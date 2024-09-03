2593d109756443f0d84b7af90d90b9ed7835e742
Therese H <therese.fredriksson90@gmail.com>
2024-09-03 12:42:03 +0200

    Added all files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", 
+    "./src/**/*.{css,js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
